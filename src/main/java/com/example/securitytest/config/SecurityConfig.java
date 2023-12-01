58334299839da0a32226a195cb55ece654674d8d
King's meow <mingee641@gmail.com>
2023-12-01 17:54:25 +0900

    refactor: 클래스 명 변경

--- src/main/java/com/example/securitytest/config/SecurityConfig.java
+++ src/main/java/com/example/securitytest/config/SecurityConfig.java
@@ -5,11 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,7 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableMethodSecurity
-public class SecurityConfiguration {
+public class SecurityConfig {
 
     @Autowired
     private JwtFilter jwtFilter;
@@ -43,9 +40,11 @@ public class SecurityConfiguration {
                                 .anyRequest().authenticated()
                 )
                 .exceptionHandling(
-                        config -> config.authenticationEntryPoint(jwtAuthEntryPoint)
+                        config -> config
+                                .authenticationEntryPoint(jwtAuthEntryPoint)
                 )
-                .sessionManagement(it -> it
+                .sessionManagement(
+                        it -> it
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
